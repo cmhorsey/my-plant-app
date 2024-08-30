a11808954adea158773151e17a7bb912d27135b0
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-30 11:58:17 -0400

    Add tests to User to check garden association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe User do
 
     expect(gardendoodle.username).to eq('gardendoodle')
   end
+
+  it 'can have many gardens' do
+    gardendoodle = User.create(username: 'gardendoodle')
+    Garden.create(name: 'vegetable garden', user: gardendoodle)
+    Garden.create(name: 'herb garden', user: gardendoodle)
+
+    expect(gardendoodle.gardens.count).to eq(2)
+  end
 end
