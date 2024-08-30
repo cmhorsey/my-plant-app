436f038dccadc112fe7854330ae69f7fe6d22a4d
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-30 11:45:51 -0400

    Update GardenPlant test spec date

--- spec/models/gardenplants_spec.rb
+++ spec/models/gardenplants_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe GardenPlant do
   subject { GardenPlant.new(
     plant_date: Date.today,
     quantity: 2,
-    last_watered: Time.now
+    last_watered: Date.today
   ) }
 
   let(:current_date) { Date.today }
-  let(:current_time) { Time.now }
 
   it { is_expected.to have_attributes(plant_date: current_date) }
   it { is_expected.to have_attributes(quantity: 2) }
-  it { is_expected.to have_attributes(last_watered: current_time) }
+  it { is_expected.to have_attributes(last_watered: current_date) }
 end
