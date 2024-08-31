897d32a2e0563630dbbe8791c353506294cec207
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-31 07:54:59 -0400

    Add Garden index and show

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,29 @@
 class GardensController < ApplicationController
+  def index
+    @gardens = Garden.all
+  end
+
+  def show
+    @garden = Garden.find(params[:id])
+  end
+
+  # def new
+  #   @garden = Garden.new
+  # end
+
+  # def create
+  #   @garden = Garden.new(garden_params)
+
+  #   if @garden.save
+  #     redirect_to garden_path, notice: "Plant was successfully created."
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+private
+
+  def plant_params
+    params.require(:plant).permit(:name, :scientific_name, :edible, :pet_safe)
+  end
 end
