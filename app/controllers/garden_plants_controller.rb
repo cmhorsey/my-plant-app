69439015a0532fc1b9b5b5edd2a169325b3150d3
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-01 09:37:57 -0400

    Add plant in garden feature

--- app/controllers/garden_plants_controller.rb
+++ app/controllers/garden_plants_controller.rb
@@ -1,2 +1,25 @@
 class GardenPlantsController < ApplicationController
+  def new
+    @garden_plant = GardenPlant.new
+    @gardens = Garden.all
+    @plants = Plant.all
+  end
+
+  def create
+    @garden_plant = GardenPlant.new(garden_plant_params)
+    @gardens = Garden.all
+    @plants = Plant.all
+
+    if @garden_plant.save
+      redirect_to gardens_path, notice: "Successfully planted."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def garden_plant_params
+    params.require(:garden_plant).permit(:last_watered, :quantity, :plant_id, :garden_id)
+  end
 end
