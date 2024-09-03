e77cae8aba4efa8acd52035bd15750f25b7b8b9b
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-03 13:31:26 -0400

    Add validations to Garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,7 @@ class Garden < ApplicationRecord
   belongs_to :user
   has_many :garden_plants
   has_many :plants, through: :garden_plants
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
 end
