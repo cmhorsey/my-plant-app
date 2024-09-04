2d9a655f71643a21d2e21754b2d34ce45bf23440
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-04 08:58:16 -0400

    Add description column to Plant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_30_195813) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_04_125704) do
   create_table "garden_plants", force: :cascade do |t|
     t.integer "garden_id", null: false
     t.integer "plant_id", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_30_195813) do
     t.boolean "pet_safe"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
