bbf5c47fa514db6124095217da132a3f4fc4e883
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-04 10:49:58 -0400

    Add quantity validations to GardenPlant model

--- app/models/garden_plant.rb
+++ app/models/garden_plant.rb
@@ -2,6 +2,8 @@ class GardenPlant < ApplicationRecord
   belongs_to :garden
   belongs_to :plant
 
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
   def formatted_plant_date
     created_at.strftime("%B %d, %Y")
   end
