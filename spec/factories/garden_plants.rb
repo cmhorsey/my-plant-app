42bf3e5a915d3c65f7ff86fd4dffb58ad755fd1d
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-05 13:49:47 -0400

    Remove unnecessary plant_date attr from GardenPlant tests

--- spec/factories/garden_plants.rb
+++ spec/factories/garden_plants.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :garden_plant do
     association :garden
     association :plant
-    plant_date { Date.today - 10 }
     quantity { 5 }
     last_watered { Date.today - 2 }
   end
--- spec/models/gardenplants_spec.rb
+++ spec/models/gardenplants_spec.rb
@@ -9,14 +9,12 @@ RSpec.describe GardenPlant, type: :model do
   end
 
   subject { GardenPlant.new(
-    plant_date: Date.today,
     quantity: 2,
     last_watered: Date.today
   ) }
 
   let(:current_date) { Date.today }
 
-  it { is_expected.to have_attributes(plant_date: current_date) }
   it { is_expected.to have_attributes(quantity: 2) }
   it { is_expected.to have_attributes(last_watered: current_date) }
 end
