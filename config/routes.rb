5a716c9369853f04fc9846f22089871a5cb4c2c5
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-05 14:07:25 -0400

    Reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  resources :garden_plants, only: [:new, :create, :update]
-  resources :plants, only: [:show, :index, :new, :create]
-
-  devise_for :users, controllers: {
-  registrations: 'users/registrations'
-}
-
   root 'home#homepage'
   get 'homepage', to: 'home#homepage'
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :users, only: [:show] do
-    resources :gardens, only: [:show, :index, :new, :create] do
-    end
+    resources :gardens, only: [:show, :index, :new, :create]
   end
+
+  resources :garden_plants, only: [:new, :create, :update]
+
+  resources :plants, only: [:show, :index, :new, :create]
 end
