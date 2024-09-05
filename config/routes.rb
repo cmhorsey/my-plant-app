be8d56ef69ee990064b463365817c22ee3e122fb
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-05 08:53:33 -0400

    Remove code comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
   registrations: 'users/registrations'
 }
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   root 'home#homepage'
   get 'homepage', to: 'home#homepage'
+  get "up" => "rails/health#show", as: :rails_health_check
 
   resources :users do
     resources :gardens do
