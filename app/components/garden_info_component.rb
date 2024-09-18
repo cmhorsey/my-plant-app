3b86717e9a8e41559fc3b1cd285a9e968fc21032
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-18 13:44:58 -0400

    Generate component files

---
+++ app/components/garden_info_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class GardenInfoComponent < ViewComponent::Base
+
+end
---
+++ test/components/garden_info_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class GardenInfoComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(GardenInfoComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
