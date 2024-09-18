496f5d8c104154bd9a6941c5a204ce590161429a
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-18 15:24:18 -0400

    Add test for formatted_date method

--- spec/components/garden_info_component_spec.rb
+++ spec/components/garden_info_component_spec.rb
@@ -5,18 +5,22 @@ RSpec.describe GardenInfoComponent, type: :component do
   let(:user) { create(:user) }
   let(:plant) { create(:plant) }
   let(:garden) { create(:garden, user: user) }
-  let(:garden_plant) { create(:garden_plant, garden: garden, plant: plant) }
+  let(:garden_plant) { create(:garden_plant, garden: garden, plant: plant, created_at: Date.new(2024, 9, 1)) }
 
   before do
     garden.garden_plants << garden_plant
     sign_in user
+    render_inline(GardenInfoComponent.new(garden: garden))
   end
 
   it "successfully renders the plants in the garden" do
-    render_inline(GardenInfoComponent.new(garden: garden))
-
     expect(page).to have_css('h3', text: "Plants you'll find here:")
     expect(page).to have_css('ul li', text: garden_plant.plant.name)
     expect(page).to have_css('.button-8', text: "Water plant 💦")
+  end
+
+
+  it "formats the plant date correctly" do
+    expect(page).to have_css('span', text: "September 01, 2024")
   end
 end
