3afc35f3340dc91423b8e6e6e687914b600c07be
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-09-18 14:07:36 -0400

    Extract plant list into view component

--- app/components/garden_info_component.rb
+++ app/components/garden_info_component.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class GardenInfoComponent < ViewComponent::Base
+  attr_reader :garden
 
+  def initialize(garden:)
+    super
+    @garden = garden
+  end
+
+  def formatted_plant_date(gardenplant)
+    gardenplant.created_at.strftime("%B %d, %Y")
+  end
 end
